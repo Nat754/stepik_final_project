444350da35030e14570b3cc8d9aac70468de397c
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-07-20 04:33:56 +0400

    add: login_page.py ref: locators.py, test_main_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.ID, "registration_link")
+
+
+class LoginPageLocators:
+    LOGIN_INPUT_EMAIL = (By.ID, "id_login-username")
+    LOGIN_INPUT_PASSWORD = (By.ID, "id_login-password")
+    LOGIN_BUTTON_LOGIN = (By.NAME, "login_submit")
+    REGISTER_INPUT_EMAIL = (By.ID, "id_registration-email")
+    REGISTER_INPUT_PASSWORD = (By.ID, "id_registration-password1")
+    REGISTER_INPUT_PASSWORD_CONFIRM = (By.ID, "id_registration-password2")
+    REGISTER_BUTTON_REGISTER = (By.NAME, "registration_submit")
+
